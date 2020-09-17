5e5c49129ddeaad16ea26dc5e9917a012139b38d
NicolasM3 <niicck992@gmail.com>
2020-09-17 08:53:30 -0300

    adicionando arvore de animacoes

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,12 @@ const acell = 10
 const gravity =300
 var   vel:Vector2 = Vector2()
 var   scene_up:Vector2 = Vector2(0, -1)
-var    anim
+var   anim
+var state_machine
+var last_direction:String = "_right"
+
+func _ready():
+	state_machine = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta) -> void:
 	var left  = Input.is_key_pressed(65)
@@ -14,7 +19,7 @@ func _physics_process(delta) -> void:
 	vel.y += gravity * delta
 
 	var direction = int(right) - int(left)
-	anim(direction)
+	animate(direction)
 	
 	if direction == -1:
 		vel.x = min(vel.x - acell, max_speed)
@@ -22,8 +27,7 @@ func _physics_process(delta) -> void:
 		vel.x = max(vel.x + acell, max_speed)
 	else:
 		vel.x = 0
-		
-			
+
 	vel = move_and_slide(vel, scene_up)
 
 func _input(event):
@@ -31,7 +35,7 @@ func _input(event):
 	if (Input.is_key_pressed(32) and just_pressed) and is_on_floor():
 		vel.y -= 200
 
-func anim(direction):
+func animate(direction):	
 	if is_on_floor() and direction == 0:
 		anim = "idle"
 	elif is_on_floor():
@@ -44,9 +48,15 @@ func anim(direction):
 			
 	if direction == 1:
 		anim += "_right"
+		last_direction = "_right"
 	elif direction == -1:
-		anim +=  "_left"
-	
-	
-	print(anim)
-	$Animation.play(anim)
+		anim += "_left"
+		last_direction = "_left"
+		
+	if(direction == 0):
+		anim += last_direction
+		
+	if Input.is_key_pressed(KEY_Q):
+		anim = "attack" + last_direction
+				
+	state_machine.travel(anim)
